0707b67796a7b999d2de1e4ed6c2aae306a98935
stannous2 <stannous2@yahoo.com>
2018-10-05 15:05:48 -0700

    added ModalForm modified BodyMain

--- client/src/pages/Yard-Sale-Listings/YardSaleListings.js
+++ client/src/pages/Yard-Sale-Listings/YardSaleListings.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from 'react-emotion';
-import { List, ListItem } from "../../components/Yard-Sale-List/index";
+import { List, ListItem } from "../../components/List/index";
 
 const YardSaleListingsWrapper = styled('div')({
     display: "flex",
