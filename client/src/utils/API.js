9beda35a91b20c748402c35b79263a6c0c3be5e2
stannous2 <stannous2@yahoo.com>
2018-10-09 15:14:06 -0700

    updates

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,10 @@ import axios from "axios";
 export default {
     getAllYardSales: function(){
         return axios.get("/api/yardSales")
+    },
+
+    //post method to insert new user
+    createNewUser: function(userData){
+        return axios.post("/api/user", userData)
     }
 }
