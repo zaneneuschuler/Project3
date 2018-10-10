e719d6b1a044bd84fe7da68acc20c790b290914c
srtalaie <srt4721@yahoo.com>
2018-10-10 16:41:39 -0700

    Yard Sales can be posted

--- client/src/pages/Yard-Sale-Creation/YardSaleCreation.js
+++ client/src/pages/Yard-Sale-Creation/YardSaleCreation.js
@@ -14,7 +14,7 @@ class YardSaleCreation extends Component {
         displayProducts: false,
         address: "Street Address City, State", 
         zipCode: 12345, 
-        yardSaleName: "Your Yard Sale's Name", 
+        name: "Your Yard Sale's Name", 
         date: "2018-12-3101:00"
     }
     
@@ -27,7 +27,7 @@ class YardSaleCreation extends Component {
 
     submitYardSale = () => {
         let newSale = {
-            yardSaleName: this.state.yardSaleName,
+            name: this.state.name,
             address: this.state.address,
             zipCode: this.state.zipCode,
             date: new Date(this.state.date + ":00Z")
@@ -49,7 +49,7 @@ class YardSaleCreation extends Component {
                 {/* Date of Sale */}
                 <YardSaleCreationElement>Date of Sale: <input type="datetime-local" name="date" value={this.state.date}onChange={this.handleInput}></input></YardSaleCreationElement>
                 {/* Name of Sale */}
-                <YardSaleCreationElement>Name of Your Sale: <input type="text" name="yardSaleName" value={this.state.yardSaleName} onChange={this.handleInput}></input></YardSaleCreationElement>
+                <YardSaleCreationElement>Name of Your Sale: <input type="text" name="name" value={this.state.name} onChange={this.handleInput}></input></YardSaleCreationElement>
 
                 <button onClick={this.submitYardSale}>Submit</button>
             </YardSaleCreationWrapper>  
