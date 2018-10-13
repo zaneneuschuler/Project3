1278ec69d5c468d0b9efb2c851b05a5703b79da2
srtalaie <srt4721@yahoo.com>
2018-10-13 13:16:35 -0700

    changed a tags to react links

--- client/src/components/Yard-Sale-List/YardSaleListCard.js
+++ client/src/components/Yard-Sale-List/YardSaleListCard.js
@@ -1,20 +1,19 @@
 import React from "react";
 import styled from "react-emotion";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./style.css"
 
 const Wrapper = styled('div')({
     margin: 10,
     boxShadow: "1px 2px 2px 0px grey",
-    padding: 10
-})
-
-const ATag = styled('a')({
-    color: "blue"
+    padding: 10,
+    color: "black"
 })
 
 export const YardSaleListCard = ({ address, zipCode, date, name, link }) => {
     return (
         <Wrapper>
-            <ATag href={"/products/" + link}>Click Here to View Products</ATag>
+            <Link className="link" to={"/products/" + link}>Click Here to View Products</Link>
             <p>{address}</p>
             <p>{zipCode}</p>
             <p>{date}</p>
