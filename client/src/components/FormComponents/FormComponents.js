1ac5b472e40690ef7106da7b93c75a08516f34da
stannous2 <stannous2@yahoo.com>
2018-10-22 06:42:47 -0700

    update App, BodyMain, Contact, UserRegistration; ADD FormComponents;  DELETE ModalFormItem component

--- client/src/components/FormComponents/FormComponents.js
+++ client/src/components/FormComponents/FormComponents.js
@@ -4,20 +4,64 @@
 import React from "react";
 import styled from 'react-emotion';
 
+export const MainForm = styled('div')({
+  width: 400,
+  height: 600,
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 20,
+  boxShadow: '0 0 15px 5px rgba(0,0,0,0.5)',
+  background: '#EDF0F3',
+  flexFlow: 'row wrap'
+})
+
+export const FormTitle = styled('div')({
+  fontSize: '1.6rem',
+  lineHeight: '1.33333',
+  color: 'rgba(0,0,0,1.9)',
+  flex: '100%',
+  background: '#F6F8FA',
+  textAlign: 'center',
+  padding: '16px 32px 8px 32px',
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '-20px -20px 0px -20px',
+  borderBottom: '1px solid rgba(0,0,0,0.15)',
+  height: 150
+})
+
+export const FormSubTitle = styled('h4')({
+  fontSize: '1.2rem',
+  lineHeight: '1.5',
+  color: 'rgba(0,0,0,1.9)',
+  flex: '100%',
+  background: '#F6F8FA',
+  textAlign: 'center',
+  position: 'relative',
+  padding: '0 8px 16px',
+  borderBottom: '1px solid rgba(0,0,0,0.15)'
+})
+
+export const FormBody = styled('formbody')({
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'center',
+  width: 360,
+})
 
 const TextBoxInput = styled('input')({
   padding: '0px 5px 5px 0',
   float: 'left',
-  margin: '5px 0 20px 0',
-  boxShadow: '0 0 1px 1px rgba(0,0,0,.3)',
-  width: 250,
+  margin: '0px 0 10px 0',
+  width: 360,
 })
+
 const TextAreaInput = styled('input')({
   padding: '0px 5px 5px 0',
   float: 'left',
   margin: '5px 0 20px 0',
-  boxShadow: '0 0 1px 1px rgba(0,0,0,.3)',
-  width: 250,
+  width: 360,
   height: 200,
   marginBottom: 5
 })
@@ -27,7 +71,14 @@ const Label = styled('div')({
   textAlign: 'left'
 })
 
-export const FormInput = ({ label, name, value, type, placeHolder, onChangeFn }) => {
+export const SubmitButton = styled('button')({
+  background: "rgb(0,115,177)",
+  color: 'white',
+  float: 'left',
+  width: 360
+})
+
+export const FormTextInput = ({ label, name, value, type, placeHolder, onChangeFn }) => {
   return (
     <div>
       <Label>{label}</Label>
--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,39 +1,13 @@
 import React, { Component } from "react";
 import styled from 'react-emotion';
-import { FormInput, FormTextArea } from "../../components/FormComponents/FormComponents";
+import {MainForm, FormTitle, FormBody, FormTextInput, FormTextArea, SubmitButton } from "../../components/FormComponents/FormComponents";
 
-import collage from '../../images/collage.jpg'
 import API from "../../utils/API";
 
 
-const BodyWrapper = styled('div')({
-  width: '100%',
-  minHeight: '100vh',
-  display: 'flex',
-  alignItems: 'center',
-  backgroundSize: 'cover',
-  background: `url("${collage}")`
-});
-
-const ContactWrapper = styled('div')({
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: 'column',
-    margin: 'auto',
-    padding: 20,
-    boxShadow: '0 0 15px 5px rgba(0,0,0,0.5)',
-    background: '#f6f8fa'
-});
-
-const SubmitButton = styled('button')({
-  padding: 8,
-  background: "rgb(0,115,177)",
-  color: 'white',
-  float: 'left',
-  width: 260
-})
-
-
+const divStyle = {
+  margin: 'auto',
+}
 class ContactForm extends Component {
   constructor() {
     super();
@@ -62,26 +36,29 @@ class ContactForm extends Component {
       };
       API.submitMessage(newMessage)
       .then(this.setState({ messageSent: true }))
-      .catch(err => console.log('fail to log in: ', err));
+      .then(console.log('messageSent state: ', this.state.messageSent)
+      )
+      .catch(err => console.log('fail to log in: ', err))
     }
   };
 
     render() {
         return (
-            <div>
+            <div style={divStyle}>
               {
                 !this.state.messageSent ?
-                <BodyWrapper>
-                  <ContactWrapper>
-                    <h3>Questions?</h3>
-                    <FormInput
-                      name="name"
-                      label="Name"
-                      type="text"
-                      onChangeFn={this.handleInputChange}
-                      value={this.state.name}
+                
+                <MainForm>
+                <FormTitle><h3>Questions?</h3></FormTitle>
+                <FormBody>
+                  <FormTextInput
+                    name="name"
+                    label="Name"
+                    type="text"
+                    onChangeFn={this.handleInputChange}
+                    value={this.state.name}
                     />
-                    <FormInput
+                    <FormTextInput
                       name="email"
                       label="Email"
                       type="text"
@@ -100,10 +77,9 @@ class ContactForm extends Component {
                 <SubmitButton onClick={this.handleMessageSubmit}>
                   Submit
                 </SubmitButton>
-                
-                  </ContactWrapper>
-                </BodyWrapper>
-                : <div><h3>Thank you for your inquiry. We will respond as soon as possible generally within a few hours. If you do not hear from us within 24 hours, kindly contact </h3> <h2>Zane Neuschuler</h2> <h3>as your message did not get to us.</h3></div>
+                </FormBody>
+              </MainForm>
+              : alert("Thank you for your inquiry. We will respond as soon as possible generally within a 24 hours.")
               }
             </div>
         );
